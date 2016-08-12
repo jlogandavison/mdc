ef9a8b3f71be9e71584f0e0bd3d00dfc6fb89d2c
Jack Davison <jack.davison@student.manchester.ac.uk>
2016-08-12 12:42:03 +0100

    Initial Commit
    * Will accept markdown files from stdin or file
    
    * Outputs HTML to stdout or file

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+//===========================================================================
+//  CMD Markdown Compiler
+//  Usage:
+//    mdc [infile] [-o outfile]
+//===========================================================================
+
+const fs = require('fs');
+const marked = require('marked');
+const minimist = require('minimist');
+
+var vargs = minimist(process.argv.slice(2));
+
+function readStream(stream, done) {
+  var data = '';
+
+  stream.resume();
+  stream.setEncoding('utf8');
+
+  stream.on('data', function(chunk) {
+    data += chunk;
+  });
+
+  stream.on('end', function(chunk) {
+    done(data);
+  });
+}
+
+function getmarkdown(done) {
+  var streams = [];
+
+  for(var i = 0; i < vargs._.length; i++) {
+    streams.push(fs.createReadStream(vargs._[i]));
+  }
+
+  if(streams.length == 0) {
+    streams.push(process.stdin);
+  }
+
+  var all_data = '';
+
+  function callback(data) {
+    all_data += data;
+    if(streams.length) {
+      readStream(streams.shift(), callback);
+    }else{
+      done(all_data);
+    }
+  }
+
+  readStream(streams.shift(), callback);
+}
+
+var outstream = (vargs.o) ? fs.createWriteStream(vargs.o) : process.stdout;
+
+getmarkdown(function(md) {
+  outstream.write(marked(md));
+});
